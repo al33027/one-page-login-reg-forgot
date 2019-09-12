fd6d38876c3d35d05c9e9ec4150eba39a20829cf
Muneco2010 <al33027@gmail.com>
2019-09-12 15:52:57 -0400

    Alejandro 09-12-19

--- js/reset-pass.js
+++ js/reset-pass.js
@@ -1,14 +1,18 @@
 function reset() {
     let resetPass = document.getElementById('reset-pass'),
         sendEmail = document.getElementById('send-email'),
-        backBtn = document.getElementById('back-btn');
+        backBtn = document.getElementById('back-btn'),
+        loginBox = document.getElementById('login-container'),
+        forgotBox = document.getElementById('forgotPass-container');
 
     backBtn.addEventListener('click', function() {
         loginBox.classList.remove('left');
-        forgotPass.classList.remove('left');
+        forgotBox.classList.remove('left');
     });
 
     sendEmail.addEventListener('click', function() {
+        loginBox.classList.remove('left');
+        forgotBox.classList.remove('left');
         console.log(resetPass.value);
     });
 }
